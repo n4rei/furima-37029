a4fe5fc14d6d967dc323b4401e651aed79aa68f1
n4rei <92794267+n4rei@users.noreply.github.com>
2022-02-23 20:11:56 +0900

    商品情報編集機能_first

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :edit_redirect_root, only: [:edit]
 
   def index
     @products = Product.all.order("created_at DESC")
@@ -22,7 +23,20 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
 
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      @product.update(product_params)
+      render :edit
+    end
+  end
 
   private
 
@@ -30,4 +44,10 @@ class ProductsController < ApplicationController
     params.require(:product).permit(:image, :price, :name_products, :explanation, :category_id, :condition_id, :delivery_id, :ship_from_id, :ship_day_id).merge(user_id: current_user.id)
   end
 
+  def edit_redirect_root
+    @product = Product.find(params[:id])
+    unless user_signed_in? && @product.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
 end
